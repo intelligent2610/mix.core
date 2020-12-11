5367cb6fd10f57c993e27d47940725b1cff27c1b
Hoang Nguyen <nhathoang989@gmail.com>
2020-12-11 08:37:33 +0700

    Init Electrolux

---
+++ src/Electrolux.Api/Controllers/ElectroluxApi.cs
@@ -0,0 +1,194 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Mix.Cms.Lib;
+using Mix.Cms.Lib.Controllers;
+using Mix.Cms.Lib.Models.Cms;
+using Mix.Cms.Lib.ViewModels.MixAttributeSetDatas;
+using Mix.Domain.Core.ViewModels;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Electrolux.Api.Controllers
+{
+    [Route("api/v1/rest/{culture}/attribute-set-data/electrolux")]
+    public class AttributeSetDataPortalController :
+         BaseRestApiController<MixCmsContext, MixAttributeSetData, FormPortalViewModel, FormPortalViewModel, FormPortalViewModel>
+    {
+        // GET: api/v1/rest/{culture}/attribute-set-data
+        [HttpGet]
+        public override async Task<ActionResult<PaginationModel<FormPortalViewModel>>> Get()
+        {
+            var getData = await Helper.FilterByKeywordAsync<FormPortalViewModel>(Request, _lang);
+            if (getData.IsSucceed)
+            {
+                return Ok(getData.Data);
+            }
+            else
+            {
+                return BadRequest(getData.Errors);
+            }
+        }
+
+        // GET: api/v1/rest/{culture}/attribute-set-data/addictional-data
+        [HttpGet("addictional-data")]
+        public async Task<ActionResult<PaginationModel<AddictionalViewModel>>> GetAddictionalData()
+        {
+            if (Enum.TryParse(Request.Query["parentType"].ToString(), out MixEnums.MixAttributeSetDataType parentType)
+                && int.TryParse(Request.Query["parentId"].ToString(), out int parentId) && parentId > 0)
+            {
+                var getData = await Helper.GetAddictionalData(parentType, parentId, Request, _lang);
+                if (getData.IsSucceed)
+                {
+                    return Ok(getData.Data);
+                }
+                else
+                {
+                    return BadRequest(getData.Errors);
+                }
+            }
+            else
+            {
+                var getAttrSet = await Mix.Cms.Lib.ViewModels.MixAttributeSets.UpdateViewModel.Repository.GetSingleModelAsync(
+                    m => m.Name == Request.Query["databaseName"].ToString());
+                if (getAttrSet.IsSucceed)
+                {
+                    AddictionalViewModel result = new AddictionalViewModel()
+                    {
+                        Specificulture = _lang,
+                        AttributeSetId = getAttrSet.Data.Id,
+                        AttributeSetName = getAttrSet.Data.Name,
+                        Status = MixEnums.MixContentStatus.Published,
+                        Fields = getAttrSet.Data.Fields,
+                        ParentType = parentType
+                    };
+                    result.ExpandView();
+                    return Ok(result);
+                }
+                return BadRequest(getAttrSet.Errors);
+            }
+        }
+
+        // PUT: api/s/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see https://aka.ms/RazorPagesCRUD.
+        [HttpPost("save-addictional-data")]
+        public async Task<IActionResult> SaveAddictionalData([FromBody] AddictionalViewModel data)
+        {
+            if (string.IsNullOrEmpty(data.Id))
+            {
+                data.CreatedBy = User.Identity.Name;
+            }
+            else
+            {
+                data.ModifiedBy = User.Identity.Name;
+                data.LastModified = DateTime.UtcNow;
+            }
+
+            var result = await base.SaveAsync<AddictionalViewModel>(data, true);
+            if (result.IsSucceed)
+            {
+                return Ok(result.Data);
+            }
+            else
+            {
+                var current = await GetSingleAsync(data.Id);
+                if (!current.IsSucceed)
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    return BadRequest(result.Errors);
+                }
+            }
+        }
+
+        // GET: api/v1/rest/{culture}/attribute-set-data
+        [HttpGet("init/{attributeSet}")]
+        public async Task<ActionResult<FormViewModel>> Init(string attributeSet)
+        {
+            int.TryParse(attributeSet, out int attributeSetId);
+            var getAttrSet = await Mix.Cms.Lib.ViewModels.MixAttributeSets.UpdateViewModel.Repository.GetSingleModelAsync(m => m.Name == attributeSet || m.Id == attributeSetId);
+            if (getAttrSet.IsSucceed)
+            {
+                FormViewModel result = new FormViewModel()
+                {
+                    Specificulture = _lang,
+                    AttributeSetId = getAttrSet.Data.Id,
+                    AttributeSetName = getAttrSet.Data.Name,
+                    Status = MixEnums.MixContentStatus.Published,
+                    Fields = getAttrSet.Data.Fields
+                };
+                result.ExpandView();
+                return Ok(result);
+            }
+            else
+            {
+                return BadRequest(getAttrSet.Errors);
+            }
+        }
+
+        // GET api/attribute-set-data
+        [HttpGet("export")]
+        public async Task<ActionResult> Export()
+        {
+            string attributeSetName = Request.Query["attributeSetName"].ToString();
+            string exportPath = $"content/exports/module/{attributeSetName}";
+            var getData = await Helper.FilterByKeywordAsync<FormPortalViewModel>(Request, _lang);
+
+            var jData = new List<JObject>();
+            if (getData.IsSucceed)
+            {
+                foreach (var item in getData.Data.Items)
+                {
+                    jData.Add(item.Obj);
+                }
+                var result = Mix.Cms.Lib.ViewModels.MixAttributeSetDatas.Helper.ExportAttributeToExcel(jData, string.Empty, exportPath, $"{attributeSetName}", null);
+                return Ok(result.Data);
+            }
+            else
+            {
+                return BadRequest(getData.Errors);
+            }
+        }
+
+        // POST api/attribute-set-data
+        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme, Roles = "SuperAdmin, Admin")]
+        [HttpPost, HttpOptions]
+        [Route("import-data/{attributeSetName}")]
+        public async Task<ActionResult<RepositoryResponse<ImportViewModel>>> ImportData(string attributeSetName, [FromForm] IFormFile file)
+        {
+            var getAttributeSet = await Mix.Cms.Lib.ViewModels.MixAttributeSets.ReadViewModel.Repository.GetSingleModelAsync(
+                    m => m.Name == attributeSetName);
+            if (getAttributeSet.IsSucceed)
+            {
+                if (file != null)
+                {
+                    var result = await Helper.ImportData(_lang, getAttributeSet.Data, file);
+                    return Ok(result);
+                }
+            }
+            return new RepositoryResponse<ImportViewModel>() { Status = 501 };
+        }
+
+        // DELETE: api/v1/rest/en-us/attribute-set/portal/5
+        [HttpDelete("{id}")]
+        public override async Task<ActionResult<MixAttributeSetData>> Delete(string id)
+        {
+            var result = await DeleteAsync<DeleteViewModel>(id, true);
+            if (result.IsSucceed)
+            {
+                return Ok(result.Data);
+            }
+            else
+            {
+                return BadRequest(result.Errors);
+            }
+
+        }
+    }
+}
--- src/Mix.Cms.Web/wwwroot/js/_custom/electrolux-register-controller.js
+++ src/Mix.Cms.Web/wwwroot/js/_custom/electrolux-register-controller.js
@@ -1,20 +1,121 @@
-app.controller('ElectroluxRegisterController',
-  ["$scope", "$controller", "ngAppSettings",
-    "RestRelatedAttributeDataPortalService",
-    "RestAttributeSetDataPortalService", function ($scope, $controller, ngAppSettings, navService, dataService) {
-      $controller('AttributeSetFormController', { $scope: $scope }); //This works
-      $scope.receipt = null;
-      $scope.initNestedData = async function () {
-        var getDefaultReceipt = await dataService.initData('thong_tin_hoa_don');
-        var getDefaultMedia = await dataService.initData('media');
-        if (getDefaultReceipt.isSucceed) {
-          $scope.receipt = getDefaultReceipt.data;
+(function (angular, $) {
+  app.controller('ElectroluxRegisterController',
+    ["$scope", "$controller", "$uibModal", "ngAppSettings",
+      "MediaService",
+      "RestRelatedAttributeDataPortalService",
+      "RestAttributeSetDataElectroluxService", function ($scope, $controller, $uibModal, ngAppSettings, mediaService, navService, dataService) {
+        $controller('AttributeSetFormController', { $scope: $scope }); //This works
+        $scope.receipt = null;
+        $scope.product = null;
+        $scope.medias = [];
+        $scope.alpha = /^[a-zA-Z\s\u0e00-\u0e7e\u4e00-\u9eff-\u007f\u0080-\u00ff\u0100-\u024f\u1e00-\u1eff\u0300-\u036f]+$/;
+        $scope.alpha_numeric = /^[a-zA-Z0-9]+$/;
+        $scope.alpha_numeric_with_minus = /^[a-zA-Z0-9\-]+$/;
+        $scope.integer = /^[-+]?\d+$/;
+        $scope.number = /^[-+]?\d*(?:[\.\,]\d+)?$/;
+        $scope.email = /^[a-zA-Z\s\u0e00-\u0e7e\u4e00-\u9eff\u0000-\u007f\u0080-\u00ff\u0100-\u024f\u1e00-\u1eff\u0300-\u036f0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z\s\u0e00-\u0e7e\u4e00-\u9eff\u0000-\u007f\u0080-\u00ff\u0100-\u024f\u1e00-\u1eff\u0300-\u036f0-9](?:[a-zA-Z\s\u0e00-\u0e7e\u4e00-\u9eff\u0000-\u007f\u0080-\u00ff\u0100-\u024f\u1e00-\u1eff\u0300-\u036f0-9-]{0,61}[a-zA-Z\s\u0e00-\u0e7e\u4e00-\u9eff\u0000-\u007f\u0080-\u00ff\u0100-\u024f\u1e00-\u1eff\u0300-\u036f0-9])?(?:\.[a-zA-Z\s\u0e00-\u0e7e\u4e00-\u9eff\u0000-\u007f\u0080-\u00ff\u0100-\u024f\u1e00-\u1eff\u0300-\u036f0-9](?:[a-zA-Z\s\u0e00-\u0e7e\u4e00-\u9eff\u0000-\u007f\u0080-\u00ff\u0100-\u024f\u1e00-\u1eff\u0300-\u036f0-9-]{0,61}[a-zA-Z\s\u0e00-\u0e7e\u4e00-\u9eff\u0000-\u007f\u0080-\u00ff\u0100-\u024f\u1e00-\u1eff\u0300-\u036f0-9])?)+$/,
+          $scope.url = /^(https?|ftp|file|ssh):\/\/(((([a-zA-Z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:)*@)?(((\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5]))|((([a-zA-Z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-zA-Z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-zA-Z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-zA-Z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-zA-Z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-zA-Z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-zA-Z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-zA-Z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?)(:\d*)?)(\/((([a-zA-Z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)+(\/(([a-zA-Z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)*)*)?)?(\?((([a-zA-Z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)|[\uE000-\uF8FF]|\/|\?)*)?(\#((([a-zA-Z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)|\/|\?)*)?$/;
+        $scope.domain = /^([a-zA-Z0-9]([a-zA-Z0-9\-]{0,61}[a-zA-Z0-9])?\.)+[a-zA-Z]{2,8}$/;
+        $scope.datetime = /^([0-2][0-9]{3})\-([0-1][0-9])\-([0-3][0-9])T([0-5][0-9])\:([0-5][0-9])\:([0-5][0-9])(Z|([\-\+]([0-1][0-9])\:00))$/;
+        $scope.date = /^(0?[1-9]|[12][0-9]|3[01])[\/\-](0?[1-9]|1[012])[\/\-]\d{4}$/;
+        $scope.init = async function (
+          formName,
+          parentId,
+          parentType,
+          validateHandler,
+          loadingHandler,
+          successHandler,
+          failHandler
+        ) {
+          $scope.successMsg = "Thành công";
+          $scope.validateHandler = validateHandler;
+          $scope.loadingHandler = loadingHandler;
+          $scope.successHandler = successHandler;
+          $scope.failHandler = failHandler;
+          $scope.formName = formName;
+          $scope.navRequest.attributeSetName = formName;
+          $scope.navRequest.parentType = parentType;
+          $scope.navRequest.parentId = parentId;
+          var getDefault = await dataService.initData($scope.formName);
+          $scope.defaultData = getDefault.data;
+          if ($scope.defaultData) {
+            $scope.defaultData.attributeSetName = $scope.formName;
+            $scope.defaultData.parentType = parentType || "Set";
+            $scope.defaultData.parentId = parentId;
+            $scope.formData = angular.copy($scope.defaultData);
+          }
+
+          $scope.$apply();
+        };
+        $scope.initNestedData = async function () {
+          // var getDefaultReceipt = await dataService.initData('thong_tin_hoa_don');
+          // var getDefaultProduct = await dataService.initData('thong_tin_san_pham');
+          // if (getDefaultReceipt.isSucceed) {
+          //   $scope.receipt = getDefaultReceipt.data;
+          // }
+          var getDefaultMedia = await dataService.initData('media');
+          if (getDefaultMedia.isSucceed) {
+            $scope.defaultMedia = getDefaultMedia.data;
+          }
+          // if (getDefaultProduct.isSucceed) {
+          //   $scope.product = getDefaultProduct.data;
+          // }
+          $scope.$apply();
         }
-        if (getDefaultMedia.isSucceed) {
-          $scope.defaultMedia = getDefaultMedia.data;
+        $scope.register = async function () {
+          const isValid = await $scope.validate(formData);
+          if (isValid) {
+            await $scope.submit($scope.formData);
+          }
         }
-      }
-      $scope.selectFiles = function () {
-        
-      }
-    }]);
+
+        $scope.selectFiles = function (files, arr) {
+          angular.forEach(files, function (e, i) {
+            $scope.openCroppie(e, arr);
+          })
+        }
+
+        $scope.uploadFile = function (file) {
+          console.log(file);
+        }
+        $scope.openCroppie = function (file, arr) {
+          var modalInstance = $uibModal.open({
+            animation: true,
+            windowClass: "show",
+            templateUrl: "/app/app-shared/components/modal-croppie/croppie.html",
+            controller: "ModalCroppieController",
+            controllerAs: "$ctrl",
+            size: "lg",
+            resolve: {
+              mediaService: mediaService,
+              file: function () {
+                return file;
+              },
+              w: 800,
+              h: 600,
+              rto: null
+            },
+          });
+
+          modalInstance.result.then(
+            function (result) {
+              let media = angular.copy($scope.defaultMedia);
+              media.obj.title = result.title;
+              media.obj.upload_file = result.fullPath;
+              arr.push(media);
+            },
+            function () { }
+          );
+        };
+
+        $scope.validate = async function (data) {
+          return true;
+        }
+      }]);
+}(angular, jQuery));
+
+
+
+
+
+
